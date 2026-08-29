2181022cc93db0448d0e6581cd706dc6bb5e80fb
vp2pj4fbz7-lang <vp2pj4fbz7@privaterelay.appleid.com>
2026-08-29 09:57:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='auto-diag-pro-v4';
+const CACHE='auto-diag-pro-v5-knowledge';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
