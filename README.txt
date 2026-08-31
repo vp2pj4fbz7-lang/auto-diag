946df784f3521e9d942930fa938572ce97debead
vp2pj4fbz7-lang <vp2pj4fbz7@privaterelay.appleid.com>
2026-08-31 14:11:46 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+Elektronik Diag V6.8 – Naprawa Smartphone
+Nowy moduł w głównym menu: 📱 Naprawa Smartphone.
+Marki: iPhone, Samsung, Xiaomi, Motorola.
+Naprawy: wyświetlacz, bateria, moduł ładowania, buzzer/głośnik multimedialny, mikrofon, głośnik rozmów.
+Każdy temat: objawy, diagnostyka, procedura naprawy, specyfika marki, porada i test końcowy.
+Wgraj index.html, RESET.html, sw.js, manifest.webmanifest. Następnie otwórz RESET.html.
--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='auto-diag-pro-v5-knowledge';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')))}else{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))}});
+const CACHE='elektronik-diag-v6-8';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request,{cache:'no-store'}));});
